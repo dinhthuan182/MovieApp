669c6b9fb4fd6ae8f164da3ad6192dba6781f88b
thuan <dinhthuan2020@gmail.com>
2020-01-31 08:47:15 +0700

    Initial Commit

---
+++ MovieApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApplication
+//
+//  Created by Catalina on 1/31/20.
+//  Copyright © 2020 Catalina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
